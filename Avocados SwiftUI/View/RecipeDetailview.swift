3ae1f9ee3c2a960b925d1e2d3fa964ec01b8d7e5
Jinu Joy <jinuparekatil@gmail.com>
2023-11-24 08:55:40 +0530

    Impliment Detail screen

---
+++ Avocados SwiftUI/View/RecipeDetailview.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailview.swift
+//  Avocados SwiftUI
+//
+//  Created by Jinu on 24/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeDetailview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailview()
+}
---
+++ Avocados SwiftUI/View/RecipeRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeRatingView.swift
+//  Avocados SwiftUI
+//
+//  Created by Jinu on 24/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeRatingView()
+}
